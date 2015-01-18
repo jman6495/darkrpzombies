268fc6910ca9fc6f826575db8761d199ba66eb73
Jordan Maris <jman6495@gmail.com>
2015-01-18 17:43:44 +0100

    Instructions added

--- LowplayerMG/lua/zombiespawns.lua
+++ LowplayerMG/lua/zombiespawns.lua
@@ -1,4 +1,5 @@
 -- This file contains a list of Vector Spawnpoints where zombies will spawn
 -- An example of a spawnpoint : spawnpoints[1] = Vector(-1748.603760, -2313.824219, -139.968750);
 -- Each spawnpoint should be numbered  from 1 upwards. in order to see your current vector coordinates ingame run Cl_showpos 1 in console
+-- you can also use getpos to print the coordinates to console. Please not the format they are printed in does NOT match the format in which they are placed here.
 spawnpoints = {}
