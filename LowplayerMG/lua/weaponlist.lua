9db02f3f47e2ff49daa593135b4414c8573e9008
Jordan Maris <jman6495@gmail.com>
2015-01-18 17:40:46 +0100

    Removed Downtown defaults for spawn locations

--- LowplayerMG/lua/weaponlist.lua
+++ LowplayerMG/lua/weaponlist.lua
@@ -1,4 +1,5 @@
 -- This file contains a list of weapons to be chosen at random and placed aroudn the map.
+-- Please remember to increment the number each time. Some demo weapons are already placed in this file. If you use custom weapons you will need to change them to the name of the entity.
 weaponlist = {}
 weaponlist[1] = "weapon_ak472";
 weaponlist[2] = "weapon_deagle2";
